a05554f6afd0eb21becb77748afa68f6ffd98193
Philip Roche <phil.roche@canonical.com>
2021-01-22 10:06:23 +0000

    Format source with black

--- src/ubuntu_package_status/ubuntu_package_status.py
+++ src/ubuntu_package_status/ubuntu_package_status.py
@@ -16,12 +16,14 @@ from launchpadlib.launchpad import Launchpad
 
 # Log in to launchpad annonymously - we use launchpad to find
 # the package publish time
-launchpad = Launchpad.login_anonymously('ubuntu-package-status', 'production', version='devel')
+launchpad = Launchpad.login_anonymously(
+    "ubuntu-package-status", "production", version="devel"
+)
 ubuntu = launchpad.distributions["ubuntu"]
 ubuntu_archive = ubuntu.main_archive
 
 # Which archive pockets are checked
-ARCHIVE_POCKETS = ['Release', 'Proposed', 'Security', 'Updates']
+ARCHIVE_POCKETS = ["Release", "Proposed", "Security", "Updates"]
 
 
 def print_package_status(package_status, output_format="TXT"):
@@ -39,71 +41,83 @@ def print_package_status_summary_txt(package_status):
         for package, pockets in package_stats.items():
             print("\t{}".format(package))
             for pocket, stats in pockets.items():
-                if stats['full_version']:
-                    print("\t\t{} {} @ {} ({})".format(
+                if stats["full_version"]:
+                    print(
+                        "\t\t{} {} @ {} ({})".format(
                             pocket,
-                            stats['full_version'],
-                            stats['date_published'],
-                            stats['date_published_formatted']
-                    ))
+                            stats["full_version"],
+                            stats["date_published"],
+                            stats["date_published_formatted"],
+                        )
+                    )
 
 
 def print_package_status_summary_csv(package_status):
     csv_stdout_writer = csv.writer(sys.stdout)
-    csv_stdout_writer.writerow([
-        "ubuntu_version",
-        "package",
-        "pocket",
-        "full_version",
-        "date_published",
-        "date_published_formatted"
-    ])
+    csv_stdout_writer.writerow(
+        [
+            "ubuntu_version",
+            "package",
+            "pocket",
+            "full_version",
+            "date_published",
+            "date_published_formatted",
+        ]
+    )
     for ubuntu_version, package_stats in package_status.items():
         for package, pockets in package_stats.items():
             for pocket, stats in pockets.items():
-                if stats['full_version']:
-                    csv_stdout_writer.writerow([
-                        ubuntu_version,
-                        package,
-                        pocket,
-                        stats['full_version'],
-                        stats['date_published'],
-                        stats['date_published_formatted']
-                    ])
+                if stats["full_version"]:
+                    csv_stdout_writer.writerow(
+                        [
+                            ubuntu_version,
+                            package,
+                            pocket,
+                            stats["full_version"],
+                            stats["date_published"],
+                            stats["date_published_formatted"],
+                        ]
+                    )
 
 
-def get_status_for_single_package(package, series, pocket, package_architecture="amd64"):
-    package_stats = {"full_version": None,
-                     "date_published": None,
-                     "date_published_formatted": None,
-                     }
+def get_status_for_single_package(
+    package, series, pocket, package_architecture="amd64"
+):
+    package_stats = {
+        "full_version": None,
+        "date_published": None,
+        "date_published_formatted": None,
+    }
     try:
         lp_series = ubuntu.getSeries(name_or_version=series)
         lp_arch_series = lp_series.getDistroArchSeries(archtag=package_architecture)
 
         package_published_binaries = ubuntu_archive.getPublishedBinaries(
-                exact_match=True,
-                binary_name=package,
-                pocket=pocket,
-                distro_arch_series=lp_arch_series,
-                status="Published",
-                order_by_date=True)
+            exact_match=True,
+            binary_name=package,
+            pocket=pocket,
+            distro_arch_series=lp_arch_series,
+            status="Published",
+            order_by_date=True,
+        )
 
         if len(package_published_binaries) > 0:
             package_published_binary = package_published_binaries[0]
             binary_package_version = package_published_binary.binary_package_version
             package_stats["full_version"] = binary_package_version
 
-            package_stats["date_published"] = \
-                package_published_binary.date_published.isoformat()
-            date_published_formatted = format_datetime(
-                    package_published_binary.date_published)
             package_stats[
-                "date_published_formatted"] = date_published_formatted
+                "date_published"
+            ] = package_published_binary.date_published.isoformat()
+            date_published_formatted = format_datetime(
+                package_published_binary.date_published
+            )
+            package_stats["date_published_formatted"] = date_published_formatted
 
     except Exception as e:
-        logging.error("Error querying launchpad API: %s. \n "
-                      "We will retry. \n", str(e))
+        logging.error(
+            "Error querying launchpad API: %s. \n " "We will retry. \n", str(e)
+        )
 
     return package_stats
 
@@ -111,24 +125,28 @@ def get_status_for_single_package(package, series, pocket, package_architecture=
 def initialize_package_stats_dict(package_config):
     package_status = dict()
 
-    default_package_stats = {"full_version": None,
-                             "date_published": None,
-                             "date_published_formatted": None,
-                             }
+    default_package_stats = {
+        "full_version": None,
+        "date_published": None,
+        "date_published_formatted": None,
+    }
 
-    default_package_versions = {'Release': default_package_stats,
-                                'Proposed': default_package_stats,
-                                'Updates': default_package_stats,
-                                'Security': default_package_stats}
+    default_package_versions = {
+        "Release": default_package_stats,
+        "Proposed": default_package_stats,
+        "Updates": default_package_stats,
+        "Security": default_package_stats,
+    }
 
-    ubuntu_versions = package_config.get('ubuntu-versions', {})
+    ubuntu_versions = package_config.get("ubuntu-versions", {})
 
     # initialise package status
     for ubuntu_version, packages in ubuntu_versions.items():
         package_list = packages.get("packages", [])
-        package_status.setdefault(ubuntu_version,
-                                  {package: default_package_versions.copy()
-                                   for package in package_list})
+        package_status.setdefault(
+            ubuntu_version,
+            {package: default_package_versions.copy() for package in package_list},
+        )
     return package_status
 
 
@@ -137,49 +155,60 @@ def get_status_for_all_packages(package_config, package_architecture="amd64"):
     for ubuntu_version, packages in package_status.items():
         for package in packages.keys():
             for pocket in ARCHIVE_POCKETS:
-                logging.info("Getting stats for {} {} {}".format(
-                        ubuntu_version, pocket.lower(), package))
-                package_stats = get_status_for_single_package(package,
-                                                              ubuntu_version,
-                                                              pocket,
-                                                              package_architecture)
+                logging.info(
+                    "Getting stats for {} {} {}".format(
+                        ubuntu_version, pocket.lower(), package
+                    )
+                )
+                package_stats = get_status_for_single_package(
+                    package, ubuntu_version, pocket, package_architecture
+                )
                 package_status[ubuntu_version][package][pocket] = package_stats
     return package_status
 
 
-@click.group()
+@click.command()
+@click.option(
+    "--config",
+    required=False,
+    default=resource_filename("ubuntu_package_status", "dist-config.yaml"),
+    help="Config yaml specifying which packages ubuntu versions to watch."
+    "{}".format(
+        " When using the ubuntu-package-status snap this"
+        " config must reside under $HOME."
+        if os.environ.get("SNAP", None)
+        else ""
+    ),
+)
+@click.option(
+    "--logging-level",
+    type=click.Choice(["DEBUG", "INFO", "WARNING", "ERROR"]),
+    required=False,
+    default="ERROR",
+    help="How detailed would you like the output.",
+)
+@click.option(
+    "--config-skeleton", is_flag=True, default=False, help="Print example config."
+)
+@click.option(
+    "--output-format", type=click.Choice(["TXT", "CSV", "JSON"]), default="TXT"
+)
+@click.option(
+    "--package-architecture",
+    help="The architecture to use when querying package "
+    "version in the archive. We use this in our Launchpad "
+    'query to query either "source" package or "amd64" package '
+    'version. Using "amd64" will query the version of the '
+    'binary package. "source" is a valid value for '
+    "architecture with Launchpad and will query the version of "
+    "the source package. The default is amd64.",
+    required=True,
+    default="amd64",
+)
 @click.pass_context
-def cli(ctx):
-    pass
-
-
-@cli.command()
-@click.option('--config', required=False, default=resource_filename(
-        'ubuntu_package_status', 'dist-config.yaml'),
-        help="Config yaml specifying which packages ubuntu versions to watch."
-             "{}".format(" When using the ubuntu-package-status snap this"
-                         " config must reside under $HOME."
-                                      if os.environ.get('SNAP', None) else ""))
-@click.option('--logging-level', type=click.Choice(['DEBUG', 'INFO',
-                                                    'WARNING', 'ERROR']),
-              required=False, default="ERROR",
-              help='How detailed would you like the output.')
-@click.option('--config-skeleton', is_flag=True, default=False,
-              help='Print example config.')
-@click.option('--output-format', type=click.Choice(['TXT', 'CSV', 'JSON']),
-              default='TXT')
-@click.option('--package-architecture',
-              help='The architecture to use when querying package '
-                   'version in the archive. We use this in our Launchpad '
-                   'query to query either "source" package or "amd64" package '
-                   'version. Using "amd64" will query the version of the '
-                   'binary package. "source" is a valid value for '
-                   'architecture with Launchpad and will query the version of '
-                   'the source package. The default is amd64.',
-              required=True, default='amd64')
-@click.pass_context
-def ubuntu_package_status(ctx, config, logging_level, config_skeleton,
-                          output_format, package_architecture):
+def ubuntu_package_status(
+    ctx, config, logging_level, config_skeleton, output_format, package_architecture
+):
     # type: (Dict, Text, Text, bool, Text, Text) -> None
     """
     Watch specified packages in the ubuntu archive for transition between
@@ -193,12 +222,13 @@ def ubuntu_package_status(ctx, config, logging_level, config_skeleton,
     # We log to stderr so that a shell calling this will not have logging
     # output in the $() capture.
     level = logging.getLevelName(logging_level)
-    logging.basicConfig(level=level, stream=sys.stderr,
-                        format='%(asctime)s [%(levelname)s] %(message)s')
+    logging.basicConfig(
+        level=level, stream=sys.stderr, format="%(asctime)s [%(levelname)s] %(message)s"
+    )
 
     # Parse config
-    with open(config, 'r') as config_file:
-        package_config = yaml.load(config_file)
+    with open(config, "r") as config_file:
+        package_config = yaml.safe_load(config_file)
         if config_skeleton:
             output = yaml.dump(package_config, Dumper=yaml.Dumper)
             print("# Sample config.")
@@ -210,5 +240,5 @@ def ubuntu_package_status(ctx, config, logging_level, config_skeleton,
     print_package_status(package_status, output_format)
 
 
-if __name__ == '__main__':
-    cli(obj={})
+if __name__ == "__main__":
+    ubuntu_package_status(obj={})

a17a408b8d73e4d709bdadc1d7a434174cb6332d
Philip Roche <phil.roche@canonical.com>
2023-04-14 15:32:46 +0100

    Bump version: 0.0.10 → 0.1.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(reqs_path, 'r') as req_file:
 
 setup(
     name='ubuntu-package-status',
-    version='0.0.10',
+    version='0.1.0',
     install_requires=dependencies,
     url='',
     license='',
